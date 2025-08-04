fe7b5b8f76af34380aa54f98e21b048159d42fa1
sᴛᴀʀ ʙᴏɪ <bgmim2890@gmail.com>
2025-08-04 17:18:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,45 +2,48 @@ from flask import Flask, request, render_template
 import requests
 from datetime import datetime
 import os
+import pytz  # For timezone
 
-app = Flask(name)
+app = Flask(__name__)
 
-#Get values from environment variables
-
+# Get values from environment variables
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
 
 def send_telegram(msg):
-url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-data = {"chat_id": CHAT_ID, "text": msg, "parse_mode": "HTML"}
-requests.post(url, data=data)
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    data = {"chat_id": CHAT_ID, "text": msg, "parse_mode": "HTML"}
+    requests.post(url, data=data)
 
 def get_ip_info(ip):
-try:
-response = requests.get(f"https://ipinfo.io/{ip}/json").json()
-return {
-"ip": ip,
-"city": response.get("city", ""),
-"region": response.get("region", ""),
-"country": response.get("country", ""),
-"loc": response.get("loc", ""),
-"org": response.get("org", "")
-}
-except:
-return {"ip": ip}
+    try:
+        response = requests.get(f"https://ipinfo.io/{ip}/json").json()
+        return {
+            "ip": ip,
+            "city": response.get("city", ""),
+            "region": response.get("region", ""),
+            "country": response.get("country", ""),
+            "loc": response.get("loc", ""),
+            "org": response.get("org", "")
+        }
+    except:
+        return {"ip": ip}
 
 @app.route('/')
 def index():
-return render_template("index.html")
+    return render_template("index.html")
 
 @app.route('/log', methods=['POST'])
 def log():
-data = request.json
-forwarded = request.headers.get('X-Forwarded-For', request.remote_addr)
-real_ip = forwarded.split(",")[0].strip()
-ip_info = get_ip_info(real_ip)
+    data = request.json
+    forwarded = request.headers.get('X-Forwarded-For', request.remote_addr)
+    real_ip = forwarded.split(",")[0].strip()
+    ip_info = get_ip_info(real_ip)
 
-msg = f"""📥 <b>New Click Logged!</b>
+    # Indian Timezone
+    india_time = datetime.now(pytz.timezone('Asia/Kolkata')).strftime('%d-%b-%Y %I:%M %p')
+
+    msg = f"""📥 <b>New Click Logged!</b>
 
 🌐 <b>IP:</b> {ip_info.get("ip")}
 🌎 <b>Country:</b> {ip_info.get("country")} | <b>City:</b> {ip_info.get("city")}
@@ -56,10 +59,10 @@ msg = f"""📥 <b>New Click Logged!</b>
 🕒 <b>Timezone:</b> {data.get("timezone")}
 🔗 <b>Referrer:</b> {data.get("referrer") or 'None'}
 
-🕒 <b>Time:</b> {datetime.now().strftime('%d-%b-%Y %I:%M %p')}
+🕒 <b>Time:</b> {india_time}
 """
-send_telegram(msg)
-return {'status': 'ok'}
+    send_telegram(msg)
+    return {'status': 'ok'}
 
-if name == 'main':
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
